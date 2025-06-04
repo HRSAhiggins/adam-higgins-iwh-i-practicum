5a45e7f6b7080548e13e2bf3add297ad19a17860
HRSAhiggins <78173837+HRSAhiggins@users.noreply.github.com>
2025-06-04 12:27:07 -0400

    Updated the route to clean up the params and the field properties. I aslso added views

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 
 app.set('view engine', 'pug');
+app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,28 +14,46 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
   
   app.get('/', async (req, res) => {
-    const committees = 'https://api.hubspot.com/crm/v3/objects/2-10391590';
+    
+    const objectType = '2-10391590'; 
+    const limit = 100;
+    const archived = false;
+    const properties = 'name,code,type,status';
+    
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectType}`; 
+    
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+    };
+    
     const params = {
-      properties : 'name, code, id',
-      limit : 100      
-    }
+      limit,
+      archived,
+      properties
+    };
+
     try {
-        const resp = await axios.get(committees, { headers });
+        const resp = await axios.get(url, { params, headers });
         const data = resp.data.results;
-        res.render('committees', { title: 'Committees', data });      
+        
+        console.log(JSON.stringify(data, null, 2));
+        
+        res.render('committees', { title: 'Committees', data });  
+        
     } catch (error) {
-        console.error(error);
+        console.error(error?.response?.data || error.message);
     }
   });
-
+  
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
+  app.get('/update-cobj', (req, res) => {
+      res.render('updates', {
+          title: 'Update Committees'
+      });
+  });
+  
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
